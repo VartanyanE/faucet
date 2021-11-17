11d3831a25e10d0c391c311d58c96b3e146c9d7f
VartanyanE <mike.vartanyan@yahoo.com>
2021-11-16 17:32:06 -0800

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+ 
+
+  networks: {
+  
+    
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7777,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+  
+  },
+
+  
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
+     
+    }
+  },
+
+  
+};

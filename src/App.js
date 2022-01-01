cbc25a8e81d90676080ca1335c986a74c7692e40
VartanyanE <mike.vartanyan@yahoo.com>
2022-01-01 15:34:36 -0800

    added web3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        provider.request({ method: "eth_requestAccounts" });
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.error("Please, install Metamask.");
+      }
+    };
+
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
   return (
-    <div className="App">
-     Hello World
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <span>
+            <strong>Account: </strong>
+          </span>
+          <h1>{account ? account : "not connected"}</h1>
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
